a085b7921c976f0a3f67ae55c75293ab9759a634
hanbing <hblx2017@163.com>
2023-03-17 22:48:05 +0800

    feign远程服务调用

--- order-service/src/main/java/cn/itcast/order/service/OrderService.java
+++ order-service/src/main/java/cn/itcast/order/service/OrderService.java
@@ -3,9 +3,9 @@ package cn.itcast.order.service;
 import cn.itcast.order.mapper.OrderMapper;
 import cn.itcast.order.pojo.Order;
 import cn.itcast.order.pojo.User;
+import cn.itcast.order.client.UserClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.RestTemplate;
 
 @Service
 public class OrderService {
@@ -14,13 +14,13 @@ public class OrderService {
     private OrderMapper orderMapper;
 
     @Autowired
-    private RestTemplate restTemplate;
+    private UserClient userClient;
 
     public Order queryOrderById(Long orderId) {
         // 1.查询订单
         Order order = orderMapper.findById(orderId);
-        String url = "http://userservice/user/" + order.getUserId();
-        User user = restTemplate.getForObject(url, User.class);
+        Long userId = order.getUserId();
+        User user = userClient.findById(userId);
         order.setUser(user);
         // 4.返回
         return order;
--- order-service/src/main/java/cn/itcast/order/web/OrderController.java
+++ order-service/src/main/java/cn/itcast/order/web/OrderController.java
@@ -9,13 +9,13 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("order")
+@RequestMapping("/order")
 public class OrderController {
 
    @Autowired
    private OrderService orderService;
 
-    @GetMapping("{orderId}")
+    @GetMapping("/{orderId}")
     public Order queryOrderByUserId(@PathVariable("orderId") Long orderId) {
         // 根据id查询订单并返回
         return orderService.queryOrderById(orderId);
