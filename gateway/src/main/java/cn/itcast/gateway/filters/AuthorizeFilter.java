77e1159f556a0a77133e752f1161b459e755260f
hblx2 <izhanlixu@163.com>
2023-03-20 13:06:08 +0800

    gateway跨域处理

--- gateway/src/main/java/cn/itcast/gateway/filters/AuthorizeFilter.java
+++ gateway/src/main/java/cn/itcast/gateway/filters/AuthorizeFilter.java
@@ -2,17 +2,19 @@ package cn.itcast.gateway.filters;
 
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.annotation.Order;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
+@Order(-1)
 @Component
 public class AuthorizeFilter implements GlobalFilter {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        MultiValueMap<String, String> queryParams = exchange.getRequest().getHeaders();
+        MultiValueMap<String, String> queryParams = exchange.getRequest().getQueryParams();
         String auth = queryParams.getFirst("authorization");
         if ("admin".equals(auth)) {
             // 放行
